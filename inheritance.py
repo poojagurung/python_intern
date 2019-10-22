0d480a6d7630be4ab00683bb67a4f56b979c12d5
Pooja Gurung <poojagurung560@gmail.com>
2019-10-22 13:47:09 +0545

    deleted a program

--- inheritance.py
+++ inheritance.py
@@ -27,28 +27,6 @@ class Equilateral(Triangle):
         self.angle3 = angle
 
 
-
-#inheriting from parent class using super
-
-class Employee(object):
-  def __init__(self, employee_name):
-    self.employee_name = employee_name
-
-  def calculate_wage(self, hours):
-    self.hours = hours
-    return hours * 20.00
-
-class PartTimeEmployee(Employee):
-  def calculate_wage(self, hours):
-    self.hours = hours
-    return hours * 12.00
-  
-  def full_time_wage(self, hours):
-    return super(PartTimeEmployee, self).calculate_wage(hours)
-
-milton = PartTimeEmployee('Milton')
-print (milton.full_time_wage(10))
-
 #simple inheritance example
 
 class Polygon():
