96136a1ce0f8f9704298a3b0968bf083e55706ce
Pooja Gurung <poojagurung560@gmail.com>
2019-10-22 16:13:21 +0545

    modified a method in example 1

--- methodoverriding.py
+++ methodoverriding.py
@@ -1,3 +1,5 @@
+#example 1
+
 class Employee(object):
   def __init__(self, employee_name):
     self.employee_name = employee_name
@@ -12,7 +14,26 @@ class PartTimeEmployee(Employee):
     return hours * 12.00
   
   def full_time_wage(self, hours):
-    return super(PartTimeEmployee, self).calculate_wage(hours)
+    return super().calculate_wage(hours)
 
 milton = PartTimeEmployee('Milton')
+print (milton.calculate_wage(10))
 print (milton.full_time_wage(10))
+
+
+#example 2
+
+class A():
+    def display(self):
+        print ("This is inside class A")
+
+    def printing(self):
+        print ("This is another method inside class A")
+
+class B(A):
+    def display(self):
+        print ("This is inside class B")
+
+a1 = B()
+a1.display()
+a1.printing()
