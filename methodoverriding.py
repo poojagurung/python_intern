3045c1e4a35add37ff75441310544ef190bd885d
Pooja Gurung <poojagurung560@gmail.com>
2019-10-22 16:44:21 +0545

    added example 2 and 3

--- methodoverriding.py
+++ methodoverriding.py
@@ -37,3 +37,19 @@ class B(A):
 a1 = B()
 a1.display()
 a1.printing()
+
+#example 3
+
+class Parent(object):
+    def __init__(self):
+        self.value = 5
+
+    def get_value(self):
+        return self.value
+
+class Child(Parent):
+    def get_value(self):
+        return self.value + 1
+
+c = Child()
+print (c.get_value())
